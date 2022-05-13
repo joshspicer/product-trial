25468d66b82bbe67ea72b88f9a39a937a5098a7a
Josh Spicer <josh@joshspicer.com>
2022-05-13 01:52:42 +0000

    config

--- .devcontainer/fetch-repos.sh
+++ .devcontainer/fetch-repos.sh
@@ -2,13 +2,13 @@
 
 # Server
 pushd /workspaces/product-trial/server
-git clone https://github.com/bookish-potato/product-trial-server.git /workspaces/product-trial/server .
+git clone https://github.com/bookish-potato/product-trial-server.git .
 npm i
 popd
 
 # Client
 pushd /workspaces/product-trial/client
-git clone https://github.com/bookish-potato/product-trial-client.git /workspaces/product-trial/client .
+git clone https://github.com/bookish-potato/product-trial-client.git  .
 npm i
 ng build
 popd
