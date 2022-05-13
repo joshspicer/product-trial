7392f74586d8976ba42ff5d54469c97ddd3c2640
Josh Spicer <josh@joshspicer.com>
2022-05-13 17:55:36 +0000

    mkdir

--- .devcontainer/fetch-repos.sh
+++ .devcontainer/fetch-repos.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Server
+mkdir -p /workspaces/product-trial/server
 pushd /workspaces/product-trial/server
 git clone https://github.com/bookish-potato/product-trial-server.git .
 npm i
 popd
 
 # Client
+mkdir -p /workspaces/product-trial/client
 pushd /workspaces/product-trial/client
 git clone https://github.com/bookish-potato/product-trial-client.git  .
 npm i
